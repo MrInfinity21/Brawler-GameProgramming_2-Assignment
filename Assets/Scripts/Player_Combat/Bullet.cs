221aa5eaa0b6faa7bc58bfd6986088554655cd66
Nithish Velu <147320824+MrInfinity21@users.noreply.github.com>
2025-06-01 13:41:02 -0600

    Made some changes

--- Assets/Scripts/Player_Combat/Bullet.cs
+++ Assets/Scripts/Player_Combat/Bullet.cs
@@ -2,18 +2,21 @@ using UnityEngine;
 
 public class ProjectileBullet : Projectile
 {
+    public float speed = 10f;
     private void Update()
     {
-        transform.Translate(Vector3.up * (projectileSpeed * Time.deltaTime));
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.CompareTag("Enemy"))
+        Debug.Log("Bullet Hit: " + other.name);
+        
+        if (other.CompareTag("Obstacle"))
         {
             Destroy(other.gameObject);
         }
-        Instantiate(impactParticles, transform.position, transform.rotation);
+        
         Destroy(gameObject);
     }
 }
